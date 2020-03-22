dacbc9afd7e8c7c86568986e8513826990acefc5
Samuel Barker <dev@sam-barker.co.uk>
2020-03-22 16:46:20 +0000

    Linting consts

--- constants.ts
+++ constants.ts
@@ -1,67 +1,67 @@
 export const TYPE_COLOURS = {
   grass: {
     PRIMARY: '#009933',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   water: {
     PRIMARY: '#0099ff',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   fire: {
     PRIMARY: '#ff9900',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   electric: {
     PRIMARY: '#ffff99',
-    SECONDARY: 'black',
+    SECONDARY: 'black'
   },
   flying: {
     PRIMARY: '#ccffff',
-    SECONDARY: 'black',
+    SECONDARY: 'black'
   },
   bug: {
     PRIMARY: '#009999',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   poison: {
     PRIMARY: '#dab3ff',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   ground: {
     PRIMARY: '#cc9900',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   rock: {
     PRIMARY: '#996633',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   steel: {
     PRIMARY: '#a6a6a6',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   dark: {
     PRIMARY: '#474747',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   ghost: {
     PRIMARY: '#666699',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   fairy: {
     PRIMARY: '#ffccff',
-    SECONDARY: 'black',
+    SECONDARY: 'black'
   },
   normal: {
     PRIMARY: '#f5f5f0',
-    SECONDARY: 'black',
+    SECONDARY: 'black'
   },
   psychic: {
     PRIMARY: '#a21a80',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   fighting: {
     PRIMARY: '#bd3900',
-    SECONDARY: 'white',
+    SECONDARY: 'white'
   },
   ice: {
     PRIMARY: '#9ce2e1',
