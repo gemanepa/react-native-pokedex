eaebc6443d7007fc1b0807eec18aefd679349317
Samuel Barker <dev@sam-barker.co.uk>
2020-04-14 14:04:47 +0100

    Removed extra describe block

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -6,21 +6,19 @@ describe('Home', () => {
     await reloadApp();
   });
 
-  describe('visual', () => {
-    it('should display the home screen on start', async () => {
-      await expect(element(by.id('home'))).toBeVisible();
-    });
-  
-    it('should have a list of Pokemon', async () => {
-      await expect(element(by.id('pokemon-list'))).toBeVisible();
-    });
-  
-    it('should have a search bar', async () => {
-      await expect(element(by.id('search-bar'))).toBeVisible();
-    });
-  
-    it('should have a copyright disclaimer', async () => {
-      await expect(element(by.id('copyright-disclaimer'))).toBeVisible();
-    });
+  it('should display the home screen on start', async () => {
+    await expect(element(by.id('home'))).toBeVisible();
+  });
+
+  it('should have a list of Pokemon', async () => {
+    await expect(element(by.id('pokemon-list'))).toBeVisible();
+  });
+
+  it('should have a search bar', async () => {
+    await expect(element(by.id('search-bar'))).toBeVisible();
+  });
+
+  it('should have a copyright disclaimer', async () => {
+    await expect(element(by.id('copyright-disclaimer'))).toBeVisible();
   });
 });
