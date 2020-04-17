df183dd166f0331c420a6045f3bd785d8e05d356
gemanepa <contact@gemanepa.com>
2020-04-17 13:12:39 -0300

    Converted PokemonListItem PureComponent to Functional

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -18,60 +18,59 @@ import { Props, State, PokemonListItemProps } from './home.types';
 import { Pokemon } from '../../types';
 /* eslint-enable no-unused-vars */
 
-class PokemonListItem extends React.PureComponent<PokemonListItemProps> {
-  render () {
-    const pokemon = this.props.data;
-    const types = pokemon.types.map(typeName => {
-      return (
-        <Image
-          key={`${pokemon.name}_${typeName}`}
-          style={styles.type}
-          source={getPokemonTypeByName(typeName).image}
-        />
-      );
-    });
 
-    const onPress = () => this.props.navigation.navigate('Details', { pokemon });
-    const testIDPrefix = `list-${pokemon.name.toLowerCase()}`;
+const PokemonListItem = React.memo(function PokemonListItem(props: PokemonListItemProps) {
+  const pokemon = props.data;
+  const types = pokemon.types.map(typeName => {
     return (
-      <TouchableOpacity
-        testID={testIDPrefix}
-        key={`${pokemon.name}_touch`}
-        onPress={onPress}
-      >
-        <View style={styles.item}>
-          <View>
-            <Text
-              testID={`${testIDPrefix}-number`}
-              style={styles.itemNumber}
-              key={pokemon.index}
-            >
-              #{pokemon.localNumber}
-            </Text>
-            <Text
-              testID={`${testIDPrefix}-name`}
-              style={styles.itemName}
-              key={pokemon.name}
-            >
-              {pokemon.name}
-            </Text>
-            <View
-              testID={`${testIDPrefix}-types`}
-              style={styles.typeContainer}
-            >
-              {types}
-            </View>
-          </View>
-          <Image
-            testID={`${testIDPrefix}-image`}
-            style={styles.itemImage}
-            source={{ uri: pokemon.sprites.home }}
-          />
-        </View>
-      </TouchableOpacity>
+      <Image
+        key={`${pokemon.name}_${typeName}`}
+        style={styles.type}
+        source={getPokemonTypeByName(typeName).image}
+      />
     );
-  }
-}
+  });
+
+  const onPress = () => props.navigation.navigate('Details', { pokemon });
+  const testIDPrefix = `list-${pokemon.name.toLowerCase()}`;
+  return (
+    <TouchableOpacity
+      testID={testIDPrefix}
+      key={`${pokemon.name}_touch`}
+      onPress={onPress}
+    >
+      <View style={styles.item}>
+        <View>
+          <Text
+            testID={`${testIDPrefix}-number`}
+            style={styles.itemNumber}
+            key={pokemon.index}
+          >
+            #{pokemon.localNumber}
+          </Text>
+          <Text
+            testID={`${testIDPrefix}-name`}
+            style={styles.itemName}
+            key={pokemon.name}
+          >
+            {pokemon.name}
+          </Text>
+          <View
+            testID={`${testIDPrefix}-types`}
+            style={styles.typeContainer}
+          >
+            {types}
+          </View>
+        </View>
+        <Image
+          testID={`${testIDPrefix}-image`}
+          style={styles.itemImage}
+          source={{ uri: pokemon.sprites.home }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+})
 
 class HomeScreen extends React.Component<Props, State> {
   constructor (props) {
@@ -96,7 +95,7 @@ class HomeScreen extends React.Component<Props, State> {
     this.setState({ searchText: text });
   }
 
-  getFilteredData (): any {
+  getFilteredData (): Pokemon[] {
     if (this.state.searchText.trim().toLowerCase() === '') return this.state.data;
     const data = this.state.data.filter(pokemon => {
       return pokemon.name.toLowerCase().includes(this.state.searchText.toLowerCase());
